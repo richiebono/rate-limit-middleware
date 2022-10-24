4548118e03e990a52adf9abb48ae7077afb210db
richiebono <richiebono@gmail.com>
2022-10-24 15:33:37 +0200

    Updated to test.

--- src/rate.limit.module.ts
+++ src/rate.limit.module.ts
@@ -3,15 +3,7 @@ import { RateLimitService } from './rate.limit.service';
 import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [CacheModule.register({
-    // @ts-ignore
-    store: async () => await redisStore({
-      socket: {
-        host: process.env.REDIS_HOST,
-        port: parseInt(process.env.REDIS_PORT),
-      }
-    })
-  })],
+  imports: [],
   providers: [RateLimitService],
 })
 export class RateLimitModule {}
